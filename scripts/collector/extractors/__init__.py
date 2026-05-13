a309cfe1ed8972b0b1ca9aee5d6311f09a10177d
cyberjuke2077 <artem.beliachkov@gmail.com>
2026-05-13 22:05:04 +0300

    feat: export new extractors (Octopart, Digi-Key, Mouser)

--- scripts/collector/extractors/__init__.py
+++ scripts/collector/extractors/__init__.py
@@ -4,5 +4,17 @@ from .base import BaseExtractor, ExtractResult
 from .chipdip import ChipDipExtractor
 from .google import GoogleExtractor
 from .nexar import NexarExtractor
+from .octopart import OctopartExtractor
+from .digikey import DigiKeyExtractor
+from .mouser import MouserExtractor
 
-__all__ = ['BaseExtractor', 'ExtractResult', 'ChipDipExtractor', 'GoogleExtractor', 'NexarExtractor']
+__all__ = [
+    'BaseExtractor',
+    'ExtractResult',
+    'ChipDipExtractor',
+    'GoogleExtractor',
+    'NexarExtractor',
+    'OctopartExtractor',
+    'DigiKeyExtractor',
+    'MouserExtractor',
+]
